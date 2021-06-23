2d3c7a9eedc2d6030ae201c98915869f19627750
Arka Das <85870164+arkadas19@users.noreply.github.com>
2021-06-23 18:02:15 +0530

    Update email_extract_data.py

--- email_extract_data.py
+++ email_extract_data.py
@@ -31,8 +31,7 @@ print("\n \n \n ----------------Inbox Mail details----------------\n")
 
 inbox = mailbox.inbox_folder()
 
-for message in inbox.get_messages(limit=1
-                                  000000000):
+for message in inbox.get_messages(limit=1000000000):
     x+=1
     #print(message.subject)
     #print("To --",message.to)
