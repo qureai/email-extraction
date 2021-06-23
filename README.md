77b43a4a7cae741be415a565e4621037fbdd7cc1
arkadas19 <arka.das@qure.ai>
2021-06-24 00:17:27 +0530

    Updated files

---
+++ README.md
@@ -0,0 +1,46 @@
+# Automated Extraction from Emails
+This repo contains the scripts and the procedure for outlook 365 users to extract all the Usernames and Email address from the CC, To , from in the inbox and To, cc,bcc from the sentbox in the MS-Outlook mailbox. 
+
+These details shall be extracted in a .csv and .xlsx format and stored in the system. The files shall contain a column beside the username and email address having a parameter 'Y' or 'N' which says whether the person is only present in sent mails or in the inbox too respectively.
+
+
+## User guide
+
+### Giving authorizations to the api
+
+The files ```giving_permissions.py``` file should be run only once. 
+Run the following commands on the terminal of your machine.
+
+These commands should be run only the first time.
+```
+pip install -r requirements.txt
+
+python giving_permissions.py 'client_id' 'client_secret' 'tenant_id'
+````
+On running the above command the user will see a message like 
+```
+Visit the following url to give consent:
+https://login.microsoftonline.com/...................
+Paste the authenticated url here:
+
+```
+
+The user should do ```Ctrl+left click``` on the url to open the page ina new tab on their browser or copy and paste it  on a new tab on their browser and press enter . **The browser should be Google chrome.** In other browsers the link may not work as intended. After signing in through qure.ai mail and then granting the permissions;
+After this copy the new url from the search bar and paste it in the terminal and press enter. 
+
+If correctly done then this message shall be displayed 
+```
+Authentication Flow Completed. Oauth Access Token Stored. You can now use the API.
+Authenticated!
+```
+If upto this correctly done the user can proceed below.
+
+
+
+
+
+### Extraction of email usernames and addresses 
+After that anytime we want to extract, the file *email_extract_data.py* will extract and store the unique usernames and emails in .xls and .csv format in *Name and Emails.xls* and *file_name.csv*.
+```
+python email_extract_data.py 'client_id' 'client_secret' 'tenant_id'
+```
---
+++ email_extract_data.py
@@ -0,0 +1,90 @@
+from O365 import Account
+import pandas as pd
+import argparse
+import time
+parser = argparse.ArgumentParser()
+parser.add_argument("client_id", type=str,
+                    help="Enter client-id here")
+parser.add_argument("client_secret", type=str,
+                    help="Enter client-secret here")
+parser.add_argument("tenant_id", type=str,
+                    help="Enter tenant-id here")
+
+args = parser.parse_args()
+
+client_id = args.client_id
+client_secret = args.client_secret
+tenant_id = args.tenant_id
+#print(type(client_id),client_secret,tenant_id)
+credentials = ( client_id ,client_secret )#These are the values you copied earlier
+# Directory (tenant) ID copied earlier
+
+account = Account(credentials, tenant_id = tenant_id) 
+
+
+data=[] #this will store all usernames and email addresses
+data_sent=[]
+mailbox = account.mailbox()
+x=0
+y=0
+print("\n \n \n ----------------Inbox Mail details----------------\n")
+
+
+inbox = mailbox.inbox_folder()
+
+seconds = time.time()
+for message in inbox.get_messages(limit=100000000,batch=300):
+    x+=1
+    #print(message.subject)
+    #print("To --",message.to)
+    for recipient in message.to._recipients:
+        data.append([str(recipient._name),str(recipient._address),'N'])
+
+    #print("Cc --",message.cc)
+    for recipient in message.cc._recipients:
+        data.append([str(recipient._name),str(recipient._address),'N'])
+    #print("Sender --")
+    data.append([str(message.sender._name),str(message.sender._address),'N'])
+print(time.time()-seconds)    
+print(x)
+
+
+
+
+
+print("\n \n \n ----------------Sent Mail details----------------\n")
+
+sent_folder = mailbox.sent_folder()
+
+
+for message in sent_folder.get_messages(limit=100000000,batch=300):
+    y+=1
+    #print(message.subject)
+    #print("To --",message.to)
+    
+    for recipient in message.to._recipients:
+        data.append([str(recipient._name),str(recipient._address),'Y'])
+
+    #print("Cc --",message.cc)    
+    for recipient in message.cc._recipients:
+        data.append([str(recipient._name),str(recipient._address),'Y'])
+
+    #print("Bcc --",message.bcc)
+    for recipient in message.bcc._recipients:
+        data.append([str(recipient._name),str(recipient._address),'Y'])
+print(y)  
+
+
+
+df = pd.DataFrame(data, columns = ['Username', 'Email','Only Sent']) #creating a pandas dataframe
+#df = df.drop_duplicates(subset=['Username','Email'],keep = 'first') #removing duplicate rows
+df.to_csv('file_name.csv', index=False)
+df.to_excel('Name and Emails.xls','Sheet1',index=False)
+'''
+for i in range(1): 
+    m = account.new_message() #creates a new mail draft
+    m.to.add('arka.das@qure.ai') #takes the email id from our file
+    m.subject = '.' #subject of the mail
+    m.body = "." 
+    m.send() # sending the mail and the loop repeats for all the selected customers #do uncomment the line for sending
+'''
